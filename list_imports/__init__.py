b3da9160e81ac9ee3a96c87b2b63f9222bc35757
Achille MARTIN <66834162+achille-martin@users.noreply.github.com>
2024-03-20 23:00:11 +0000

    Prevented Syntax error from breaking the list generation

--- list_imports/__init__.py
+++ list_imports/__init__.py
@@ -13,11 +13,14 @@ def get(path):
 def parse(code):
     """return a list of python code imports"""
     imports = set()
-    tree = ast.parse(code)
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            for subnode in node.names:
-                imports.add(subnode.name)
-        if isinstance(node, ast.ImportFrom):
-            imports.add(('.' * node.level) + str(node.module))
+    try:
+        tree = ast.parse(code)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for subnode in node.names:
+                    imports.add(subnode.name)
+            if isinstance(node, ast.ImportFrom):
+                imports.add(('.' * node.level) + str(node.module))
+    except Exception as e:
+        print("EXCEPTION CAUGHT: {}".format(str(e)))
     return list(imports)
